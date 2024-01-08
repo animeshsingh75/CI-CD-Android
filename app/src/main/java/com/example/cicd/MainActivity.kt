32fe4d7b0ce7c2e84ce6f94387c8c78ae673cbbb
Animesh <animeshsingh75@gmail.com>
2024-01-08 04:24:35 -0700

    Initial commit

---
+++ app/src/main/java/com/example/cicd/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.cicd
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cicd.databinding.ActivityMainBinding
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        AppCenter.start(
+            application,
+            "3abf3928-cdf3-4312-8245-d1e87b69ec76",
+            Analytics::class.java,
+            Crashes::class.java
+        )
+    }
+}
